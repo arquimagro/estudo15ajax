0db34ddc42b76a2c3fe1f874ff8217c57496a6b5
lucas <lucas.e.a.cipriano@gmail.com>
2024-04-10 18:28:42 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,61 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//     document.getElementById('btn-buscar-cep').addEventListener('click', function(){
+//         const xhttp = new XMLHttpRequest();
+//         const cep = document.getElementById('cep').value;
+//         const endPoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//         xhttp.open('GET', endPoint);
+//         xhttp.send();
+//     })
+// })
+
+$(document).ready(function(){
+    $('#cep').mask('00000-000')
+
+    $('#btn-buscar-cep').click(function(){
+        const cep = $('#cep').val();
+        const endPoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
+
+        // $.ajax(endPoint).done(function(resposta){
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //     $('#endereco').val(endereco);
+
+        //     setTimeout(function(){
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 1000)
+        // })
+
+        fetch(endPoint).then(function(resposta){
+            return resposta.json
+        })
+            .then(function(json){
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+                $('#endereco').val(endereco);
+            })
+            .catch(function(erro){
+                alert("ocorreu um erro ao buscar o endereço")
+            })
+            .finally(function(){
+                setTimeout(function(){
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+                }, 1000)
+            })
+
+            
+    })
+})
+
